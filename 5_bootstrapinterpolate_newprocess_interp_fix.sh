667d4c9670211abe571991b3f6f3a5caf234901e
nickill <nicholas.killerby-smith@anu.edu.au>
2020-08-24 21:26:15 +1000

    edited the interpolation method to match Naomi's description in a way that i think is right and may work but will confirm with her first.

---
+++ 5_bootstrapinterpolate_newprocess_interp_fix.sh
@@ -0,0 +1,68 @@
+#!/bin/csh
+
+cd /priv/myrtle1/gaskap/nickill/1934project/uvfiles
+
+set log = (${log})
+
+date |& tee -a ${log}
+
+set gpcalint = (1)
+set gpcalintgap = (0.5)
+set gpcalnfbin = (1)
+
+
+#only april and may 2017 and 2018 have 1419.5.6 data and they are unusable as a result so 1419.5.6 need not be considered 
+#should it be required later just add a line identical to instances of 1419.5.2 with the names changed below it
+
+#flagging channels varies with time of year
+foreach dest ( 2016-04*.uv.split 2016-05*.uv.split 2016-08*.uv.split 2016-11*.uv.split 2017-02*.uv.split 2019*.uv.split )
+	echo "STARTING ${dest}" |& tee -a ${log}
+	cd /priv/myrtle1/gaskap/nickill/1934project/uvfiles/${dest}
+	pwd |& tee -a ${log}
+	#remove preivous calibrations
+	rm -rf interpolated |& tee -a ${log}
+	rm -rf bootstrap |& tee -a ${log}
+	#start cal
+	mkdir interpolated |& tee -a ${log}
+	mkdir bootstrap |& tee -a ${log}
+	#move data into the bootstrap directory
+	cp -r 1934-638.14* bootstrap/ |& tee -a ${log}
+	cd /priv/myrtle1/gaskap/nickill/1934project/uvfiles/${dest}/bootstrap
+	#make copy of the 1420 for flagging
+	cp -r 1934-638.1420.5.* 1934-638.1420.5.6_flaggedHI |& tee -a ${log}
+
+ 	#flag the HI line on the new copy
+	if ( ${dest} =~ 2016-04*.uv.split ) uvflag vis=1934-638.1420.5.6_flaggedHI line=channel,230,3000,1,1 flagval=f |& tee -a ${log}
+	if ( ${dest} =~ 2016-05*.uv.split ) uvflag vis=1934-638.1420.5.6_flaggedHI line=channel,230,3000,1,1 flagval=f |& tee -a ${log}
+	if ( ${dest} =~ 2016-08*.uv.split ) uvflag vis=1934-638.1420.5.6_flaggedHI line=channel,200,3370,1,1 flagval=f |& tee -a ${log}
+	if ( ${dest} =~ 2016-11*.uv.split ) uvflag vis=1934-638.1420.5.6_flaggedHI line=channel,200,3390,1,1 flagval=f |& tee -a ${log}
+	if ( ${dest} =~ 2017-02*.uv.split ) uvflag vis=1934-638.1420.5.6_flaggedHI line=channel,300,2990,1,1 flagval=f |& tee -a ${log}
+	if ( ${dest} =~ 2019*.uv.split ) uvflag vis=1934-638.1420.5.6_flaggedHI line=channel,200,3080,1,1 flagval=f |& tee -a ${log}
+
+	#copy HI flagged 1420 and unflagged in HI 1420 into interpolated folder for calibration, 1419 unneeded
+	cp -r 1934-638.1420.5.* ../interpolated/ |& tee -a ${log}
+
+	#calibrate interpolated method, ordering is quite different to bootstrap below
+	cd /priv/myrtle1/gaskap/nickill/1934project/uvfiles/${dest}/interpolated
+	mfcal vis=1934-638.1420.5.6_flaggedHI/ line=channel,5200,400,1,1 refant=3 interval=5 options=interpolate |& tee -a ${log}
+	gpcal vis=1934-638.1420.5.6_flaggedHI/ line=channel,5200,400,1,1 refant=3 interval=$gpcalint options=xyvary nfbin=$gpcalnfbin |& tee -a ${log}
+	gpcopy vis=1934-638.1420.5.6_flaggedHI/ out=1934-638.1420.5.6/ |& tee -a ${log} #repeated because wildcards not supported in out parameter
+	if ( ${dest} =~ 2019*.uv.split ) gpcopy vis=1934-638.1420.5.6_flaggedHI/ out=1934-638.1420.5.4/ |& tee -a ${log} # 2019 has different ID tag for 1420
+	cp -rf 1934-638.1420.5.6_flaggedHI/bandpass 1934-638.1420.5.6/bandpass |& tee -a ${log}
+	if ( ${dest} =~ 2019*.uv.split ) cp -rf 1934-638.1420.5.6_flaggedHI/bandpass 1934-638.1420.5.4/bandpass |& tee -a ${log}
+	
+	#now do the bootstrapping
+	cd /priv/myrtle1/gaskap/nickill/1934project/uvfiles/${dest}/bootstrap 
+	mfcal vis=1934-638.1419.5.2 line=channel,5200,400,1,1 refant=3 interval=5 |& tee -a ${log}
+	mfcal vis=1934-638.1420.5.6_flaggedHI/ line=channel,5200,400,1,1 refant=3 interval=5 |& tee -a ${log}
+	cp -rf 1934-638.1419.5.2/bandpass 1934-638.1420.5.6_flaggedHI/bandpass |& tee -a ${log}
+	delhd in=1934-638.1420.5.6_flaggedHI/gains |& tee -a ${log}
+	mfcal vis=1934-638.1420.5.6_flaggedHI/ line=channel,5200,400,1,1 refant=3 interval=5 options=nopassol |& tee -a ${log}
+	gpcal vis=1934-638.1420.5.6_flaggedHI/ line=channel,5200,400,1,1 refant=3 interval=$gpcalint options=xyvary nfbin=$gpcalnfbin |& tee -a ${log}
+	if ( ${dest} =~ 2019*.uv.split ) gpcopy vis=1934-638.1420.5.6_flaggedHI/ out=1934-638.1420.5.4/ |& tee -a ${log} # 2019 has different ID tag for 1420
+	gpcopy vis=1934-638.1420.5.6_flaggedHI/ out=1934-638.1420.5.6/ |& tee -a ${log} #repeated because wildcards not supported in out parameter
+	echo "FINISHED ${dest}" |& tee -a ${log}
+end
+
+echo "EVERYTHING IS DONE" |& tee -a ${log}
+
