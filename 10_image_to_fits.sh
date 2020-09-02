65526f127eb741a4c509b68dadb8ea4e8786eaf4
nickill <nicholas.killerby-smith@anu.edu.au>
2020-09-02 13:37:39 +1000

    corrected directory

--- 10_image_to_fits.sh
+++ 10_image_to_fits.sh
@@ -2,12 +2,12 @@
 
 #miriad task to crop in space and velocity
 
-cd /priv/myrtle1/gaskap/nickill/uvfiles/images
+cd /priv/myrtle1/gaskap/nickill/1934project/uvfiles/images
 
 foreach filename ( 1420_bootstrap_merge_restored 1420_interpolated_merge_restored )
 
     fits in=${filename}.map out=${filename}.fits op=xyout line=velocity
-    
+
 end
 
 
